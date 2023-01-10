ce48a704fdb20bf8599528025d30e7d4682ff9a8
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-01-10 23:14:14 +0530

    Program 1 completed

---
+++ program_1/positive.py
@@ -0,0 +1,7 @@
+number = float(input(" Please Enter any Numeric Value: "))
+if (number > 0):
+    print(format(number) + " is a Positive Numeber")
+elif(number < 0):
+    print(format(number) + " is a Negative Number")
+else:
+    print("You have entered Zero")
