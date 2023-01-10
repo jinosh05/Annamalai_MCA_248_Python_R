41152eb61ea3e29bf4a8706c08d1a11ac4992153
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-01-10 23:18:00 +0530

    Program 3 completed

---
+++ program_3/armstrong_number.py
@@ -0,0 +1,11 @@
+num = input("Enter number to check for Armstrong:")
+result = 0
+temp = int(num)
+while(temp > 0):
+    remainder = int(temp % 10)
+    temp = temp/10
+    result = result+(remainder*remainder*remainder)
+if(int(result) == int(num)):
+    print("Given number " + num + " is an Armstrong number.")
+else:
+    print("Given number " + num + " is not an Armstrong number.")
