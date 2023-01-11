159637aea4ece37ad84da226347d5bf4bd6ea64b
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-01-11 21:13:42 +0530

    Program_6 completed

---
+++ program_6/check_odd.R
@@ -0,0 +1,7 @@
+i <- readline(prompt="Enter a number: ")
+# Convert string in to integer
+x <- as.integer(i)
+# Time to check odd or even
+if( (x%%2) == 0) {
+print(paste("The number ",i," is Even"))
+} else {print(paste("The number ",i," is Odd"))}
